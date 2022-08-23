2795710e624699b11c0787d90b2cdf0d59d5e8b2
Yago Fogaça <yagofogaca20@gmail.com>
2022-08-23 16:58:07 -0300

    feat: create config(p) / update service user

--- services/user.service.js
+++ services/user.service.js
@@ -1,11 +1,11 @@
-export const getAll = async () => {
-    return await userDb.find();
-};
-
-export const getById = async (id) => {
-    return await userDb.findOne({ id: id });
-};
-
-export const create = async (user) => {
-    return await userDb.create(user);
-};
+export class userService {
+    async getAll() {
+        return await userDb.find();
+    }
+    async getById(id) {
+        return await userDb.findOne({ id: id });
+    }
+    async create(user) {
+        return await userDb.create(user);
+    }
+}
