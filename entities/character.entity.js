73ec64a4b4e517c55d10f4ead127615964c65490
Yago Fogaça <yagofogaca20@gmail.com>
2022-08-23 14:38:38 -0300

    feat: config id character entity

--- entities/character.entity.js
+++ entities/character.entity.js
@@ -1,11 +1,21 @@
+import { randomUUID } from "node:crypto";
+
 export class CharacterEntity {
-    constructor(character, userName) {
-        this.userName = userName.name;
+    constructor(character, user) {
+        this.id;
+        this.userName = user.userName;
         this.name = character.name;
         this.image = character.image;
     }
+    createId() {
+        const random = randomUUID();
+        const idUser = random.substring(0, 7);
+        this.id = idUser;
+        return idUser;
+    }
     printCharacter() {
         return {
+            id: this.id,
             userName: this.userName,
             name: this.name,
             image: this.image,
