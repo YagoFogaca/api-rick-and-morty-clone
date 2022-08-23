cc9cba63e82362f77e65b8f630c5d8a759327d68
Yago Fogaça <yagofogaca20@gmail.com>
2022-08-23 15:14:19 -0300

    feat: connection database

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+import mongo from "mongoose";
+const { connect } = mongo;
+
+import { config } from "dotenv";
+config();
+
+// import mongo from "mongoose";
+// const { connect } = mongo;
+
+export function connectDatabase() {
+    connect(process.env.BASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => console.log("Connected to database"))
+        .catch((err) => console.log(err));
+}
