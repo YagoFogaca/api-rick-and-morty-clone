fccd18bb6a80d08f512b3ed7c9a358a162b08fa5
Yago Fogaça <yagofogaca20@gmail.com>
2022-08-24 16:13:22 -0300

    feat: updae route user and install express

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,10 +2,10 @@ import { getAll, create } from '../controllers/user.controller.js';
 
 import { Router } from 'express';
 
-export const router = Router();
+const router = Router();
 
 router.get('/', getAll);
 
 router.post('/create', create);
 
-// export default router;
+export default router;
