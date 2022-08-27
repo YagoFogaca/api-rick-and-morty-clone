7f79cba3b30bc7671aeaae59324e69eb134a3f16
Yago Fogaça <yagofogaca20@gmail.com>
2022-08-27 12:39:04 -0300

    feat: update erros controller

--- controllers/errors/erroController.js
+++ controllers/errors/erroController.js
@@ -1,15 +1,16 @@
 import { newError } from './newError.js';
 
 export function errors(data) {
-  if (data.length === 0) {
-    throw new newError({ status: 204, message: 'No content' });
-  }
-
+  console.log(data);
   if (!data) {
     throw new newError({ status: 404, message: 'Not Found' });
   }
 
   if (data === false) {
     throw new newError({ status: 400, message: 'Bad Request' });
+  }
+
+  if (data.length === 0) {
+    throw new newError({ status: 204, message: 'No content' });
   }
 }
